25b3675c1b8e9fee36cf3bf042b98c0c56da2c01
张大川 <dachuanz@gmail.com>
2017-03-29 15:33:16 +0800

    22

--- bubble.c
+++ bubble.c
@@ -60,12 +60,62 @@ void choice(int *a,int n)
 	}
 
 }
+/**
+
+快速排序 
+*/
+
+void quick(int *a,int i,int j)
+
+{
+
+	int m,n,temp;
+
+	int k;
+
+	m=i;
+
+	n=j;
+
+	k=a[(i+j)/2]; /*选取的参照*/
+
+	do {
+
+		while(a[m]<k&&m<j) {
+			m++;   /* 从左到右找比k大的元素*/
+		}
+
+		while(a[n]>k&&n>i) {
+			n--;   /* 从右到左找比k小的元素*/
+		}
+
+		if(m<=n) { /*若找到且满足条件，则交换*/
+
+			temp=a[m];
+
+			a[m]=a[n];
+
+			a[n]=temp;
+
+			m++;
+
+			n--;
+
+		}
+
+	} while(m<=n);
+
+	if(m<j) quick(a,m,j); /*运用递归*/
+
+	if(n>i) quick(a,i,n);
+
+}
 
 
 int main() {
 
 	int e []  = {9,2,3,4,5,88,};
-	choice(e,6);
+	quick(e,0,5);
 	int i;
 	for( i=0; i<6; i++)
 
